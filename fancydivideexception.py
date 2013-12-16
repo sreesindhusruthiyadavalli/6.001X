9b60aa0129d7438b17e244c947051d5385adf62c
sreesindhusruthiyadavalli <sssindh@gmail.com>
2013-12-17 01:32:54 +0530

    Create fancydivideexception.py

---
+++ fancydivideexception.py
@@ -0,0 +1,13 @@
+def FancyDivide(numbers,index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError, e:
+        print "-1"
+    else:
+        print "1"
+    finally:
+        print "0"
+        
+FancyDivide([0, 2, 4], 0)       
