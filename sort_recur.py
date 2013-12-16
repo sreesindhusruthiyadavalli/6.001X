cf887dea8021a3eb035a5ff9e45fbb4dcecc9dd6
sreesindhusruthiyadavalli <sssindh@gmail.com>
2013-12-17 01:59:25 +0530

    Create sort_recur.py

---
+++ sort_recur.py
@@ -0,0 +1,24 @@
+def sort4(lst):
+    def unite(l1, l2):
+        if len(l1) == 0:
+            return l2
+        elif len(l2) == 0:
+            return l1
+        elif l1[0] < l2[0]:
+            return [l1[0]] + unite(l1[1:], l2)
+        else:
+            return [l2[0]] + unite(l1, l2[1:])
+
+    if len(lst) == 0 or len(lst) == 1:
+        return lst
+    else:
+        front = sort4(lst[:len(lst)/2])
+        back = sort4(lst[len(lst)/2:])
+
+        L = lst[:]  # the next 3 questions below refer to this line
+        print L
+        return unite(front, back)
+        
+lst = [3,5,42,1,0]      
+print sort4(lst)
+print lst  
