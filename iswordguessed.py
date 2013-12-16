0df3f412ccc6c84554f3da8cab4e255b69124b5a
sreesindhusruthiyadavalli <sssindh@gmail.com>
2013-12-17 01:43:46 +0530

    Create iswordguessed.py

---
+++ iswordguessed.py
@@ -0,0 +1,17 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    
+    # FILL IN YOUR CODE HERE...
+    for char in secretWord:
+        if char not in lettersGuessed:
+            return False
+    return True
+
+secretWord = 'apple' 
+lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+print isWordGuessed(secretWord, lettersGuessed)
