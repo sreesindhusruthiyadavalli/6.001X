95837866e45af6c65136f3e539ed89c9a2e37759
sreesindhusruthiyadavalli <sssindh@gmail.com>
2013-12-17 02:00:24 +0530

    Create sort2_smallest first.py

---
+++ sort2_smallest first.py
@@ -0,0 +1,14 @@
+def sort2(lst):
+    for iteration in range(len(lst)):
+        minIndex = iteration
+        minValue = lst[iteration]
+        for j in range(iteration+1, len(lst)):
+            if lst[j] < minValue:
+                minIndex = j
+                minValue = lst[j]
+        temp = lst[iteration]
+        lst[iteration] = minValue
+        lst[minIndex] = temp
+
+        L = lst[:]  # the next 3 questions below refer to this line
+    return lst
