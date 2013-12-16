3774515a9aa3c43b30cd0be140eac977c4b27449
sreesindhusruthiyadavalli <sssindh@gmail.com>
2013-12-17 01:21:35 +0530

    Create Frobfindfront.py

---
+++ Frobfindfront.py
@@ -0,0 +1,9 @@
+def findFront(start):
+    """
+    start: a Frob that is part of a doubly linked list
+    returns: the Frob at the beginning of the linked list 
+    """
+    # Your Code Here
+    if start.getBefore() == None:
+        return start
+    return findFront(start.getBefore())    
