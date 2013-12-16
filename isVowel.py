eb2bd6f4bf1f3767f28e8180b789e856bfae0840
sreesindhusruthiyadavalli <sssindh@gmail.com>
2013-12-17 01:42:51 +0530

    Create isVowel.py

---
+++ isVowel.py
@@ -0,0 +1,10 @@
+def isVowel(char):
+    '''
+    char: a single letter of any case
+
+    returns: True if char is a vowel and False otherwise.
+    '''
+    # Your code here
+    return bool(char == 'a' or char == 'e' or char == 'i' or char == 'o' \
+                or char == 'u' or char == 'A' or char == 'E' or char == 'I'\
+                or char == 'O' or char == 'U')   
